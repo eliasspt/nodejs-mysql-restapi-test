749eacb073941c84e3108534a02887912257b0b3
eliasspt <eliasspt@gmail.com>
2023-04-19 10:12:10 -0400

    start script añadido

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ import { config } from "dotenv";
 
 config();
 
-export const PORT = process.env.PORT  || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "eliasspt";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "maofcr12";
-export const DB_DATABASE = process.env.DB_DATABASE = "companydb";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const PORT = process.env.PORT;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PORT = process.env.DB_PORT;
